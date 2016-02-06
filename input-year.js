40bba1ee82ca8c0f640530074f8fc24b32dffdeb
Andrew Fan <andrewsuicune@gmail.com>
2016-02-06 12:45:05 -0500

    Added potentially broken untested directive for year validation

---
+++ input-year.js
@@ -0,0 +1,29 @@
+(function(){
+    var app = angular.module('inputyear', [ ]);
+
+    var INTEGER_REGEXP = /^\-?\d+$/;
+
+    app.directive('inputYear', function() {
+    	return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, elm, attrs, ctrl) {
+              ctrl.$validators.inputYear = function(modelValue, viewValue) {
+                if (ctrl.$isEmpty(modelValue)) {
+                  // consider empty models to be valid
+                  return true;
+                }
+
+                if (INTEGER_REGEXP.test(viewValue) && viewValue > 1851) {
+                  // it is valid
+                  return true;
+                }
+
+                // it is invalid
+                return false;
+              };
+            }
+    	};
+    });
+})();
+
