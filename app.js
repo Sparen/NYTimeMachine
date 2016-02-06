c34ac17a934cefb76e7024bf5a0b5ef25d81dabe
John Amadeo Daniswara <john.amadeo@hotmail.co.uk>
2016-02-06 13:57:49 -0500

    Implemeted timeline controller

--- app.js
+++ app.js
@@ -7,10 +7,47 @@
     app.controller('TimelineController', ['$http', function($http){
         var timeline = this;
 
-        $http.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=Indonesia&fq=news_desk:("Business")&api-key=sample-key')
+        timeline.headline = "";
+        timeline.startYear = 0;
+        timeline.endYear = 0;
+
+        timeline.span = timeline.endYear - timeline.startYear;
+        timeline.hits = 0;
+        timeline.hitsPerYear = 0;
+
+        timeline.results = [];
+
+        /*timeline.update = function(headline, startYear, endYear){
+        	timeline.headline = headline;
+        	timeline.startYear = startYear;
+        	timeline.endYear = endYear;
+        };*/
+
+        timeline.showResults = function(){
+        	console.log(timeline.results);
+        };
+
+        timeline.submit = function(){
+        	$http.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?q='
+        			+ timeline.headline 
+        			+ '&begin_date=' 
+        			+ timeline.startYear
+        			+ '0101&end_date='
+        			+ timeline.endYear
+        			+ '0101&sort=oldest&fl=abstract%2Clead_paragraph%2Cheadline%2Cpub_date&api-key=db6c22023a90449345e4d9e999dabb02:2:74312658')
             .success(function(data){
-            timeline.testData = data.response.docs[0].abstract;
-        });
+	            timeline.results = data.response.docs;
+	            timeline.hits = timeline.results.length;
+	            timeline.hitsPerYear = timeline.hits / timeline.span;
+        	});
+        };
+
+
+		//	http://api.nytimes.com/svc/search/v2/articlesearch.json?q=Indonesia&begin_date=19970101&end_date=20150101&fl=abstract%2Clead_paragraph%2Cheadline%2Cpub_date&api-key=sample-key
+    
     }]);
 })();
+
+
+//ng-model='timeline.headline' ng-click='update(timeline.headline, timeline.startYear, timeline.endYear)'
 
